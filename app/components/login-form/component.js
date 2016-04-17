3b059ef8d85a2700020d963b25f6c20131231931
Ryan LaBouve <imryanlabouve@gmail.com>
2016-04-17 17:20:56 -0500

    Sub out actions for login and singout

--- app/components/login-form/component.js
+++ app/components/login-form/component.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  session: Ember.inject.service()
+  session: Ember.inject.service(),
+  actions: {
+    login() {
+      return true;
+    },
+    signout() {
+      return true;
+    }
+  }
 });
