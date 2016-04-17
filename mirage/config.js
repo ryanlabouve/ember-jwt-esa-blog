ffdd071f87a16dd6f5b64313016d1661b80bfa2f
Ryan LaBouve <imryanlabouve@gmail.com>
2016-04-17 16:23:27 -0500

    Setup mirage with private posts

--- mirage/config.js
+++ mirage/config.js
@@ -1,24 +1,21 @@
 export default function() {
+  this.get('/public-posts');
+  this.post('/knock/auth', (db, request) =>  {
+    const req = JSON.parse(request.requestBody);
+    const pw = Ember.get(req, 'auth.password');
 
-  // These comments are here to help you get started. Feel free to delete them.
-
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-  */
+    if(pw === 'test1234') {
+      return new Mirage.Response(201, {}, { jwt: 'hotdog' });
+    } else {
+      return new Mirage.Response(404, {}, {});
+    }
+  });
+  this.get('/private-posts', ({ privatePost }, request) => {
+    const token = Ember.get(request, 'requestHeaders.Authorization');
+    if (token === 'hotdog') {
+      return privatePost.all();
+    } else {
+      return new Mirage.Response(401, {}, {});
+    }
+  });
 }
---
+++ mirage/factories/private-post.js
@@ -0,0 +1,7 @@
+import { Factory, faker } from 'ember-cli-mirage';
+
+export default Factory.extend({
+    body: faker.lorem.words(),
+    title: faker.lorem.paragraphs(),
+    createdAt: faker.date.past()
+});
