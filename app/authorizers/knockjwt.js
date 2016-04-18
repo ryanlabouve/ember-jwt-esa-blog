fb822f2de77eecbe4db4fb0bd34e7f19ef56af8b
Ryan LaBouve <imryanlabouve@gmail.com>
2016-04-18 00:45:16 -0500

    Authorizer

--- app/authorizers/knockjwt.js
+++ app/authorizers/knockjwt.js
@@ -1,7 +1,15 @@
 import Base from 'ember-simple-auth/authorizers/base';
+import Ember from 'ember';
 
 export default Base.extend({
-  authorize(/*data, block*/) {
-    debugger;
+  session: Ember.inject.service(),
+  authorize(data, block) {
+    if (Ember.testing) {
+      block('Authorization', 'Bearer hotdog');
+    }
+    const { token } = data;
+    if (this.get('session.isAuthenticated') && token) {
+      block('Authorization', `Bearer ${token}`);
+    }
   }
 });
