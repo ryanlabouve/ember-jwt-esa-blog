82af23b7044f9783995468c34f7cd9e9c688bafc
Ryan LaBouve <imryanlabouve@gmail.com>
2016-04-18 00:31:29 -0500

    Add application route mixin

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ApplicationRouteMixin, {
   model() {
     return this.store.findAll('publicPost');
   }
